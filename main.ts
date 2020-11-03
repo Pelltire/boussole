55225a17f8311af1e79210b0dd66e9d8089eed69
Pelltire <73190861+Pelltire@users.noreply.github.com>
2020-11-03 14:12:20 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,16 +3,16 @@ input.calibrateCompass()
 basic.forever(function () {
     direction = input.compassHeading()
     if (direction < 45) {
-        basic.showArrow(ArrowNames.North)
+        basic.showString("N")
     } else {
         if (direction < 135) {
-            basic.showArrow(ArrowNames.East)
+            basic.showString("E")
         } else {
             if (direction < 225) {
-                basic.showArrow(ArrowNames.South)
+                basic.showString("S")
             } else {
                 if (direction < 315) {
-                    basic.showArrow(ArrowNames.West)
+                    basic.showString("W")
                 }
             }
         }
